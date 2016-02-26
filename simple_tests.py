fbe71e54b8727211dff7cb215c93177406b99a81
arkilic <armanarkilic@gmail.com>
2016-02-26 14:16:38 -0500

    MNT:Updated dummy test script

--- simple_tests.py
+++ simple_tests.py
@@ -1,6 +1,8 @@
 from amostra.client.commands import SampleReference, RequestReference
 import uuid
 import time
+from requests.exceptions import HTTPError
+
 m_sample = dict(name='m_sample', uid=str(uuid.uuid4()), 
                 time=time.time(), owner='arkilic', project='trial',
                 beamline_id='trial_b')
@@ -9,20 +11,14 @@ s1 = SampleReference([m_sample],
 r = s1.add(name='2nd')
 print('Sample has been created', r)
 crsr = s1.find(name='m_sample')
-s1.update(query={'uid':m_sample['uid']}, update={'time':0.00003})
-
+try:
+	s1.update(query={'uid':m_sample['uid']}, update={'time':0.00003})
+except HTTPError:
+	print('Caught the appropriate update error')
 req1 = RequestReference(sample=m_sample, host='localhost', port=7770)
 req1.create(sample=m_sample, host='localhost', port=7770)
-
 s2 = SampleReference()
-
-print(m_sample['uid'])
-req1.update(query={'sample': m_sample['uid']}, update={'time': 1})
-
-print(next(req1.find(time=1)))
-print(next(s1.find(time=0.00003)))
-
-t = time.time()
-
-res = s1.update(query={'time': 0.00003}, update={'time': t})
-print(list(s1.find(time=t)))
+print('Trying to update reference state')
+req1.update(query={'sample': m_sample['uid']}, update={'state': 'inactive'})
+if next(req1.find(state='inactive')):
+	print('Successfully updated the request state')
