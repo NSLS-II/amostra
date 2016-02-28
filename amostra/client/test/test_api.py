42a05b70f284e039cc55fe215377c37520938a7e
arkilic <armanarkilic@gmail.com>
2016-02-28 12:01:57 -0500

    WIP/TST:Structuring for adding unti tests

---
+++ amostra/client/test/test_api.py
@@ -0,0 +1,28 @@
+# Smoketest the api
+
+from collections import deque
+import time as ttime
+import datetime
+import pytz
+from nose.tools import assert_equal, assert_raises, raises, assert_true
+
+sample_uid = None
+document_insert_time = None
+
+def tearDown():
+    pass
+
+def setup():
+    global sample_uid, document_insert_time
+    document_insert_time = ttime.time()
+    
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    import nose
+    nose.runmodule(argv=['-s', '--with-doctest'], exit=False)
---
+++ amostra/client/test/test_commands.py
@@ -0,0 +1,9 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+from collections import deque
+import time as ttime
+import datetime
+import pytz
+from nose.tools import assert_equal, assert_raises, raises, assert_true
+import metadataclient.commands as mdsc
+import uuid
