707479c3c7c9d73f4546f9fd4624d42e2a3c390e
arkilic <armanarkilic@gmail.com>
2016-02-29 11:08:43 -0500

    FIX:Command line parsing ambiguity fix leftover work

--- amostra/ignition.py
+++ amostra/ignition.py
@@ -9,7 +9,16 @@ from amostra.server.conf import load_configuration
 
 
 def start_server(config=None):
-    print('Here i am')
+    """
+    Returns
+    Parameters
+    ----------
+    config: dict
+        Command line arguments always have priority over local config or
+        yaml files.Using these parameters,  a tornado event loop is created.
+        Keep in mind that this server is started in lazy fashion. It does not verify
+        the existence of a mongo instance running on the specified location.
+    """
     if not config:
         config = {k: v for k, v in load_configuration('amostra', 'AMST',
                                                       ['mongo_host', 'mongo_port', 'timezone',
@@ -31,23 +40,22 @@ def start_server(config=None):
     if args.database is not None:
         config['database'] = args.database
     if args.mongo_host is not None:
-        config['mongo_host'] = args.host
+        config['mongo_host'] = args.mongo_host
     if args.timezone is not None:
         config['timezone'] = args.timezone
     if args.mongo_port is not None:
-        config['mongo_port'] = args.port
+        config['mongo_port'] = args.mongo_port
     service_port = args.service_port
     if service_port is None:
         service_port = 7770
-    print(config)
     db = db_connect(config['database'],
                     config['mongo_host'],
                     config['mongo_port'])
-    print(db)
     application = tornado.web.Application([
         (r'/sample', SampleReferenceHandler),
         (r'/request', RequestReferenceHandler),
         (r'/schema', SchemaHandler)
          ], db=db)
+    print('Starting Amostra service on ', config)
     application.listen(service_port)
     tornado.ioloop.IOLoop.current().start()
