c606e738da27f17f9ea6a573f012091d23db196a
ke-zhang-rd <kezhang@bnl.gov>
2019-08-15 15:29:35 -0400

    STY: flake8

--- amostra/tests/test_revert.py
+++ amostra/tests/test_revert.py
@@ -1,22 +1,24 @@
 from hypothesis import given, strategies as st
-from hypothesis.strategies import text
 from hypothesis import settings
 import random
 import string
 
 
-@given(names = st.lists(st.text(alphabet=string.ascii_lowercase, min_size=1, max_size=4), min_size=3, max_size=4, unique=True))
-@settings(max_examples = 10, deadline = 1000)
+@given(names=st.lists(st.text(alphabet=string.ascii_lowercase,
+                              min_size=1, max_size=4),
+                      min_size=3, max_size=4, unique=True))
+@settings(max_examples=10, deadline=1000)
 def test_revert(client, names):
     n = len(names)
-    s = client.samples.new(name = names[0])
+    s = client.samples.new(name=names[0])
     uuid = s.uuid
     for name in names[1:]:
-        s.name = name 
+        s.name = name
 
     num = random.randint(0, n-2)
 
-    revert_target_cursor = client._db.samples_revisions.find({'revision': num, 'uuid': uuid})
+    revert_target_cursor = client._db.samples_revisions.find({'revision': num,
+                                                              'uuid': uuid})
     s.revert(num)
     target = next(revert_target_cursor)
     for name, trait in s.traits().items():
