048e67f95efd56b01bd630b765259885ecf7d56b
ke-zhang-rd <kezhang@bnl.gov>
2019-08-15 12:07:56 -0400

    TST: using one db

--- amostra/tests/conftest.py
+++ amostra/tests/conftest.py
@@ -1,7 +1,14 @@
+import amostra.mongo_client
 import pytest
+import uuid
+from pymongo import MongoClient
 
 
 @pytest.fixture()
-def url():
+def client():
     url = 'mongodb://localhost:27017/'
-    return url
+    db_name = str(uuid.uuid4())
+    client = amostra.mongo_client.Client(url + db_name)
+    yield client
+    #Clean db at the end of whole test function
+    MongoClient(url).drop_database(db_name)
--- amostra/tests/test_revert.py
+++ amostra/tests/test_revert.py
@@ -14,18 +14,16 @@ for i in range(26):
 
 @given(names = st.lists(st.text(alphabet=alphabet_list, min_size=1, max_size=4), min_size=3, max_size=4, unique=True))
 @settings(max_examples = 10, deadline = 1000)
-def test_revert(url, names):
-    db_name = str(uuid.uuid4())
-    client = amostra.mongo_client.Client(url + db_name)
-
+def test_revert(client, names):
     n = len(names)
     s = client.samples.new(name = names[0])
+    uuid = s.uuid
     for name in names[1:]:
-        s.name = name
+        s.name = name 
 
     num = random.randint(0, n-2)
 
-    revert_target_cursor = client._db.samples_revisions.find({'revision': num})
+    revert_target_cursor = client._db.samples_revisions.find({'revision': num, 'uuid': uuid})
     s.revert(num)
     target = next(revert_target_cursor)
     for name, trait in s.traits().items():
@@ -33,5 +31,3 @@ def test_revert(url, names):
             continue
         else:
             assert getattr(s, name) == target[name]
-
-    MongoClient(url).drop_database(db_name)
