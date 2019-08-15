c9a07ccf60d6a5c83b778b3138fb7ddfca82b528
ke-zhang-rd <kezhang@bnl.gov>
2019-08-15 15:22:52 -0400

    FIX: slight issue

--- amostra/tests/test_revert.py
+++ amostra/tests/test_revert.py
@@ -1,18 +1,11 @@
-import amostra.mongo_client
 from hypothesis import given, strategies as st
 from hypothesis.strategies import text
 from hypothesis import settings
-from pymongo import MongoClient
 import random
-import uuid
+import string
 
 
-alphabet_list = ''
-for i in range(26):
-    alphabet_list = alphabet_list + chr(97 + i)
-
-
-@given(names = st.lists(st.text(alphabet=alphabet_list, min_size=1, max_size=4), min_size=3, max_size=4, unique=True))
+@given(names = st.lists(st.text(alphabet=string.ascii_lowercase, min_size=1, max_size=4), min_size=3, max_size=4, unique=True))
 @settings(max_examples = 10, deadline = 1000)
 def test_revert(client, names):
     n = len(names)
@@ -27,7 +20,7 @@ def test_revert(client, names):
     s.revert(num)
     target = next(revert_target_cursor)
     for name, trait in s.traits().items():
-        if name is 'revision':
+        if name == 'revision':
             continue
         else:
             assert getattr(s, name) == target[name]
