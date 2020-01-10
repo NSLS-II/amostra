982860dd7be7c523107473f5e14bd82a3cdfc6a8
ke-zhang-rd <kezhang@bnl.gov>
2020-01-10 10:39:46 -0500

    TST: Update file path location

--- amostra/tests/test_openapi.py
+++ amostra/tests/test_openapi.py
@@ -1,7 +1,10 @@
 import json
+import os
 
 
 def test_load():
-    with open('../../docs/source/openapi.json') as f:
+    here = os.path.abspath(os.path.dirname(__file__))
+    loc = os.path.join(here, '../../docs/source/openapi.json')
+    with open(loc) as f:
         data = json.loads(f.read())
         assert data
