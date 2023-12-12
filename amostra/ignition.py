269b3ab7d1632c555b59feb69018f0dd5db04b7f
Jun Aishima <jaishima@bnl.gov>
2023-12-12 13:31:18 -0500

    change to using URI only

--- amostra/ignition.py
+++ amostra/ignition.py
@@ -27,58 +27,36 @@ def start_server(config=None):
     """
     if not config:
         config = {k: v for k, v in load_configuration('amostra', 'AMST',
-                                                      ['mongo_host',
-                                                       'mongo_port', 'timezone',
+                                                      ['mongo_uri',
+                                                       'timezone',
                                                        'database',
                                                        'service_port'],
                                                       allow_missing=True).items() if v is not None}
     parser = argparse.ArgumentParser()
     parser.add_argument('--database', dest='database', type=str,
                         help='name of database to use')
-    parser.add_argument('--mongo-host',
-                         dest='mongo_host', type=str,
-                        help='host to use')
+    parser.add_argument('--mongo-uri',
+                         dest='mongo_uri', type=str,
+                        help='URI for connecting to central MongoDB')
     parser.add_argument('--timezone', dest='timezone', type=str,
                         help='Local timezone')
-    parser.add_argument('--mongo-port', dest='mongo_port', type=int,
-                        help='port to use to talk to mongo')
     parser.add_argument('--service-port', dest='service_port', type=int,
                         help='port listen to for clients')
-    parser.add_argument('--no-auth', dest='auth', action='store_false')
-    parser.add_argument('--auth', dest='auth', action='store_true')
-    parser.set_defaults(auth=False)
-    parser.add_argument('--mongo-user', dest='mongo_user', type=str,
-                            help='Mongo username')
-    parser.add_argument('--mongo-pwd', dest='mongo_pwd', type=str,
-                            help='Mongo password')
     args = parser.parse_args()
     print(args)
     if args.database is not None:
         config['database'] = args.database
-    if args.mongo_host is not None:
-        config['mongo_host'] = args.mongo_host
+    if args.mongo_uri is not None:
+        config['mongo_uri'] = args.mongo_uri
+    else:
+        raise KeyError("mongo_uri must be defined")
     if args.timezone is not None:
         config['timezone'] = args.timezone
-    if args.mongo_port is not None:
-        config['mongo_port'] = args.mongo_port
     service_port = args.service_port
     if service_port is None:
         service_port = 7770
-    if args.auth:
-        if args.mongo_user and args.mongo_pwd:
-            config['mongo_user'] = args.mongo_user
-            config['mongo_pwd'] = args.mongo_pwd
-        else:
-            raise KeyError('--mongo-user and --mongo-pwd required with auth')
-    else:
-        config['mongo_user'] = None
-        config['mongo_pwd'] = None
     db = db_connect(database=config['database'],
-                    mongo_host=config['mongo_host'],
-                    mongo_port=config['mongo_port'],
-                    mongo_user=config['mongo_user'],
-                    mongo_pwd=config['mongo_pwd'],
-                    auth=args.auth)
+                    mongo_host=config['mongo_uri'])
     application = tornado.web.Application([
         (r'/sample', SampleReferenceHandler),
         (r'/request', RequestReferenceHandler),
