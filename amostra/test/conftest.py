7921c5185feb97eabb5fe3ed15b03b7224490ee6
Jun Aishima <jaishima@bnl.gov>
2024-02-01 08:57:33 -0500

    fix naming of fixture objects
     * fix to amostra, since that is what we are testing here

---
+++ amostra/test/conftest.py
@@ -0,0 +1,49 @@
+import uuid
+import pytest
+import subprocess
+import contextlib
+import time as ttime
+import sys
+
+from amostra.client.commands import AmostraClient
+
+testing_config = {
+    "database": "mds_testing_disposable_{}".format(str(uuid.uuid4())),
+    "mongo_server": "localhost",
+    "mongo_uri": "mongodb://localhost",
+    "mongo_port": 27017,
+    "host": "localhost",
+    "port": 7770,
+    "timezone": "US/Eastern",
+    "mongo_user": "tom",
+    "mongo_pwd": "jerry",
+    "local_files": "~/amostra_files",
+}
+
+
+@contextlib.contextmanager
+def amostra_startup():
+    try:
+        ps = subprocess.Popen(
+            [
+                sys.executable,
+                "-c",
+                f"from amostra.ignition import start_server; start_server(config={testing_config}, testing=True)",
+            ],
+        )
+        ttime.sleep(1.3)  # make sure the process is started
+        yield ps
+    finally:
+        ps.terminate()
+
+
+@pytest.fixture(scope="session")
+def amostra_server():
+    with amostra_startup() as amostra_fixture:
+        yield
+
+
+@pytest.fixture(scope="function")
+def amostra_client():
+    conn = AmostraClient(host=testing_config["host"], port=testing_config["port"])
+    return conn
